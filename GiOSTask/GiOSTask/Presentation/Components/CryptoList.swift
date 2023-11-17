8a90eaeaf70b955ac2888aa72a1d839215916a46
Lakshminaidu <lakshminaidu118@gmail.com>
2023-11-17 22:21:15 +0530

    - List header has been added

--- GiOSTask/GiOSTask/Presentation/Components/CryptoList.swift
+++ GiOSTask/GiOSTask/Presentation/Components/CryptoList.swift
@@ -11,16 +11,29 @@ struct CryptoList: View {
     var cryptoArr: [CryptoData]
     var cryptoIcons: [String: CryptoIconModel]
     var body: some View {
-        List {
-            ForEach(cryptoArr, id: \.id) { data in
-                let cryptoId = data.id
-                CryptoCell(icon: cryptoIcons["\(data.id)"]?.logo ?? "" , cryptoData: data)
-                    .listRowSeparator(.hidden)
+        VStack {
+            HStack {
+                TextElement(text: "Top Currencies", fontSize: 16, font: AppFont.InterBold.rawValue, alignment: .leading)
+                    .foregroundColor(Color.black)
+                Spacer()
+                TextElement(text: "View All", fontSize: 12, font: AppFont.InterBold.rawValue, alignment: .trailing)
+                    .foregroundColor(Color.filterBtnColor)
             }
+            .padding([.horizontal, .top], 16)
+            .padding([.bottom], 4)
+            
+            List {
+                ForEach(cryptoArr, id: \.id) { data in
+                    let cryptoId = data.id
+                    CryptoCell(icon: cryptoIcons["\(data.id)"]?.logo ?? "" , cryptoData: data)
+                        .listRowSeparator(.hidden)
+                }
+            }
+            .listStyle(.plain)
+            .background(Color.clear)
+            .listRowBackground(Color.clear)
         }
-        .listStyle(.plain)
-        .background(Color.clear)
-        .listRowBackground(Color.clear)
+        
     }
 }
 
